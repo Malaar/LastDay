41adb24c40b746b53ed30a96a0ee378f6e7603b6
Malaar <korshilovskiy@gmail.com>
2011-03-15 22:00:30 +0200

    First commit on Git

---
+++ Classes/LastDayAppDelegate.h
@@ -0,0 +1,32 @@
+//
+//  LastDayAppDelegate.h
+//  LastDay
+//
+//  Created by Malaar on 15.03.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface LastDayAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSURL *)applicationDocumentsDirectory;
+- (void)saveContext;
+
+@end
+
